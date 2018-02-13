9d39a4e705cfd6954a7f36563741fe8ae6889a24
martinkobelka <martinkobelka@gmail.com>
2018-02-14 00:38:41 +0100

    Add configuration files for intellij IDEA

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,16 +7,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    /**
+     * Title of applcation windows
+     */
+    private static final String TITLE = "Moje super kalkulačka";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle(Main.TITLE);
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
+    /**
+     * This method is called after start of program
+     *
+     * @param args Arguments of command line
+     */
     public static void main(String[] args) {
         launch(args);
     }
