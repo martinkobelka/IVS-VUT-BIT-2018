0eadb14da19d67810a7b6df43104349d3c74d2f6
j-anonym <vavrojj@gmail.com>
2018-03-02 20:11:54 +0100

    Basic tests of "ADD" and "SUBSTRACT" operations.

--- src/tests/My_math_interfaceTest.java
+++ src/tests/My_math_interfaceTest.java
@@ -9,15 +9,12 @@ import static org.junit.Assert.*;
 
 public class My_math_interfaceTest {
 
+    private My_math_interface my_math;
+    private Operation operation;
+
     @Before
     public void setUp() throws Exception {
-
-        /**
-         *
-         * Set up informations
-         *
-         *
-         */
+        my_math = new My_math();
 
     }
 
@@ -31,23 +28,63 @@ public class My_math_interfaceTest {
     /**
      *
      * There will be test for another operation and its arguments
-     *
+     */
+
     @Test
-    public void operate() {
-      ........
-      ..........
+    public void contant_pi() throws Exception {
+        assertEquals(Math.PI, my_math.return_constant(Type_of_constant.PI));
     }
 
-     @Test
-     public void operate_2() {
-     ........
-     ..........
-     }
-     *
-     *
-     *
-     *
-     *
-     *
-     */
+    @Test
+    public void contant_euler() throws Exception {
+        assertEquals(Math.E, my_math.return_constant(Type_of_constant.EULER_NUMBER));
+    }
+
+    @Test
+    public void add() throws Exception {
+        double[] operands = {5, 2};
+        operation = Operation.ADD;
+        assertEquals(10, my_math.run_operate(operands, operation));
+        operands = {2, -2};
+        assertEquals(0, my_math.run_operate(operands, operation));
+        operands = {-10, -15};
+        assertEquals(-25, my_math.run_operate(operands, operation));
+    }
+
+    @Test(expected = MathException.class)
+    public void add_over_limit() throws Exception {
+        double[] operands = {Double.MAX_VALUE, 1};
+        operation = Operation.ADD;
+        my_math.run_operate(operands, operation);
+    }
+
+    @Test(expected = MathException.class)
+    public void add_under_limit() throws Exception {
+        double[] operands = {Double.MIN_VALUE, -1};
+        operation = Operation.ADD;
+        my_math.run_operate(operands, operation);
+    }
+
+    @Test
+    public void substract() throws Exception {
+        double[] operands = {10, 10};
+        operation = Operation.SUBTRACT;
+        assertEquals(0, my_math.run_operate(operands, operation));
+
+    }
+
+    @Test(expected = MathException.class)
+    public void substract_over_limit() throws Exception {
+        double[] operands = {Double.MAX_VALUE, -1};
+        operation = Operation.SUBTRACT;
+        my_math.run_operate(operands, operation);
+    }
+
+    @Test(expected = MathException.class)
+    public void substract_under_limit() throws Exception {
+        double[] operands = {Double.MIN_VALUE, 1};
+        operation = Operation.SUBTRACT;
+        my_math.run_operate(operands, operation);
+    }
+
 }
