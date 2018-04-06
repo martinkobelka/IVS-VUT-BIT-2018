c049c9b00e623c8a3cae9e9cb7e8dc7408bbf4d6
j-anonym <vavrojj@gmail.com>
2018-04-06 18:49:06 +0200

    Added auxiliary methods, default language and fixed "==" with .equal() in class LanguageRepresentation

--- src/translator/Translator.java
+++ src/translator/Translator.java
@@ -4,6 +4,8 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -12,11 +14,27 @@ import java.util.List;
 public class Translator implements Translator_interface {
 
     private String language;
+    private String default_language;
+    private LanguageRepresetnation language_content;
+    private LanguageRepresetnation default_language_content;
 
-    @Override
+    public Translator() {
+        default_language = getDefaultLanguage();
+        if (default_language != null) {
+            language = default_language;
+            default_language_content = new LanguageRepresetnation();
+            loadFromFile();
+        }
+    }
+
     public void setLanguage(String language) throws LanguageException {
-        List<String> languages;
-        languages = getLanguages();
+
+        if (language.equals(default_language.substring(1))) {
+            this.language = default_language;
+            return;
+        }
+
+        List<String> languages = getLanguages();
         boolean is_supported = false;
 
         for (String tmp : languages) {
@@ -29,48 +47,102 @@ public class Translator implements Translator_interface {
         } else {
             throw new LanguageException(LanguageException.LanguageExceptionType.LANGUAGE_NOT_FOUND);
         }
+
+        language_content = new LanguageRepresetnation();
+        loadFromFile();
     }
 
-    @Override
     public String translate(String department, String item) throws LanguageException {
-        XMLInputFactory factory = XMLInputFactory.newInstance();
-        XMLStreamReader xsr = null;
-
-        if (language == null) {
+        if (language == null && default_language == null) {
             throw new LanguageException(LanguageException.LanguageExceptionType.LANGUAGE_NOT_SET);
         }
 
-        String path = "./languages/" + language + ".xml";
-        String element = "";
-        String tmp = "";
-        boolean depfound = false;
+        String result;
+
+        if (default_language.equals(language)) {
+            try {
+                result = default_language_content.getItem(department, item);
+            } catch (LanguageException ex) {
+                throw new LanguageException(LanguageException.LanguageExceptionType.NO_TRANSLATION);
+            }
+            return result;
+        }
+
+        try {
+            result = language_content.getItem(department, item);
+        } catch (LanguageException e) {
+            try {
+                result = default_language_content.getItem(department, item);
+            } catch (LanguageException ex) {
+                throw new LanguageException(LanguageException.LanguageExceptionType.NO_TRANSLATION);
+            }
+            return result;
+        }
+        return result;
+    }
+
+    private List<String> getFileNames() {
+        List<String> languages = new ArrayList<>();
+        URL lang_path = this.getClass().getResource("./languages");
+        try {
+            Files.newDirectoryStream(Paths.get(lang_path.getPath()), path -> path.toString().endsWith(".xml")).forEach(filePath -> languages.add(filePath.toString().substring(filePath.toString().lastIndexOf('/') + 1, filePath.toString().length() - 4)));
+        } catch (IOException e) {
+        }
+
+        return languages;
+    }
+
+    public List<String> getLanguages() {
+        List<String> fileNames = getFileNames();
+        List<String> languages = new ArrayList<>();
+
+        for (String fileName : fileNames) {
+            if (fileName.startsWith("_")) {
+                String tmp = fileName.substring(1);
+                languages.add(tmp);
+            } else {
+                languages.add(fileName);
+            }
+        }
+        return languages;
+    }
+
+    private void loadFromFile() {
+        String department = null, item = null, value = null, element = null;
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        XMLStreamReader xsr = null;
+
+        URL lang_path = this.getClass().getResource("./languages");
+        String path = lang_path.getPath() + "/" + language + ".xml";
 
         try {
             xsr = factory.createXMLStreamReader(new FileReader(path));
-            //-------------------------------------------------------------//
             while (xsr.hasNext()) {
                 if (xsr.getEventType() == XMLStreamConstants.START_ELEMENT) {
                     element = xsr.getName().getLocalPart();
                     if (element.equals("department")) {
-                        tmp = xsr.getAttributeValue(0);
-                        if (tmp.equals(department)) {
-                            depfound = true;
-                        }
+                        department = xsr.getAttributeValue(0);
+                    } else if (element.equals("item")) {
+                        item = xsr.getAttributeValue(0);
                     }
 
-                } else if (depfound && xsr.getEventType() == XMLStreamConstants.CHARACTERS) {
+                } else if (xsr.getEventType() == XMLStreamConstants.CHARACTERS) {
                     if (element.equals("item")) {
-                        if (xsr.getAttributeValue(0).equals(item)) {
-                            return xsr.getText();
-                        }
+                        value = xsr.getText();
                         element = "";
                     }
+                } else if ((xsr.getEventType() == XMLStreamConstants.END_ELEMENT)) {
+                    if ((xsr.getName().getLocalPart().equals("item"))) {
+                        if (language.equals(default_language)) {
+                            default_language_content.addItem(department, item, value);
+                        } else {
+                            language_content.addItem(department, item, value);
+                        }
+                    }
                 }
+
                 xsr.next();
             }
-            setLanguage("_english");
-            translate(department, item);
-            throw new LanguageException(LanguageException.LanguageExceptionType.NO_TRANSLATION);
 
         } catch (Exception e) {
 
@@ -80,17 +152,15 @@ public class Translator implements Translator_interface {
             } catch (Exception e) {
             }
         }
-        return null;
     }
 
-    public List<String> getLanguages() throws LanguageException {
-        List<String> languages = new ArrayList<>();
-        Files.newDirectoryStream(Paths.get("./languages"),
-                path -> path.toString().endsWith(".xml")).forEach(filePath -> languages.add(filePath.toString()));
-
-        for (String tmp : languages) {
-            tmp = tmp.substring(tmp.lastIndexOf('/') + 1, tmp.length() - 4);
+    private String getDefaultLanguage() {
+        List<String> filenames = getFileNames();
+        for (String tmp : filenames) {
+            if (tmp.startsWith("_")) {
+                return tmp;
+            }
         }
-        return languages;
+        return null;
     }
 }
