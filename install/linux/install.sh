5cad18e6d1ac46511edfdbf557f9d74d2d8413de
martinkobelka <martinkobelka@gmail.com>
2018-04-24 23:45:17 +0200

    Add doc

--- install/linux/install.sh
+++ install/linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo mkdir /usr/lib/my_007_calculator
-sudo cp -r ./out/pack/* /usr/lib/my_007_calculator
+sudo cp -r ./assets* /usr/lib/my_007_calculator
 sudo cp ./my_007_calculator /usr/bin/my_007_calculator
 sudo cp ./icon.png /usr/lib/my_007_calculator/icon.png
 sudo chmod 777 /usr/bin/my_007_calculator
@@ -10,7 +10,7 @@ while getopts "d" arg; do
     d)
 
       sudo cp launcher_my_007_calculator.desktop ~/Desktop/launcher_my_007_calculator.desktop &> /dev/null
-      sudo chmod 777 ~/Desktop/launcher_my_007_calculator.desktop > /dev/null
+      sudo chmod 777 ~/Desktop/launcher_my_007_calculator.desktop &> /dev/null
 
       sudo cp launcher_my_007_calculator.desktop ~/Plocha/launcher_my_007_calculator.desktop &> /dev/null
       sudo chmod 777 ~/Plocha/launcher_my_007_calculator.desktop &> /dev/null
